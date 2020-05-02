c22333499f77a7aebe4605d47cc9e9f93cfeb0f3
Julia <jlchen6@wisc.edu>
2020-05-01 23:41:58 -0500

    Updates

---
+++ precache-manifest.863ecf4cd1603fd2f8d7484f0762b966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21888f7105e3968e27c6d9e5f409856c",
+    "url": "/employee-directory-gh-pages/index.html"
+  },
+  {
+    "revision": "80f04ed9a1a760fa9817",
+    "url": "/employee-directory-gh-pages/static/css/main.67a36697.chunk.css"
+  },
+  {
+    "revision": "3d04914c0a9a969bfaae",
+    "url": "/employee-directory-gh-pages/static/js/2.8a4eb8ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory-gh-pages/static/js/2.8a4eb8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f04ed9a1a760fa9817",
+    "url": "/employee-directory-gh-pages/static/js/main.dbc429f4.chunk.js"
+  },
+  {
+    "revision": "e63fdb90911a1e91a801",
+    "url": "/employee-directory-gh-pages/static/js/runtime-main.b4324820.js"
+  }
+]);
